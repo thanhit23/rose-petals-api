366567cab28cf07b3cfe9c12f2f7a38fa96584d0
Duy Thanh <duythanhit203@gmail.com>
2023-07-31 15:24:04 +0700

    [Feature] config res auth v1

--- src/controllers/v1/auth.controller.js
+++ src/controllers/v1/auth.controller.js
@@ -1,4 +1,3 @@
-const httpStatus = require('http-status');
 const catchAsync = require('../../utils/catchAsync');
 const { roles } = require('../../config/roles');
 const { authService, userService, tokenService, emailService } = require('../../services/app');
@@ -6,46 +5,46 @@ const { authService, userService, tokenService, emailService } = require('../../
 const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
   const tokens = await tokenService.generateAuthTokens(user);
-  res.status(httpStatus.CREATED).send({ user, tokens });
+  res.createSuccess({ user, tokens });
 });
 
 const login = catchAsync(async (req, res) => {
   const { email, password } = req.body;
   const user = await authService.loginUserWithEmailAndPasswordAndRole(email, password, roles.user);
   const tokens = await tokenService.generateAuthTokens(user);
-  res.send({ user, tokens });
+  res.success({ user, tokens });
 });
 
 const logout = catchAsync(async (req, res) => {
   await authService.logout(req.body.refreshToken);
-  res.status(httpStatus.NO_CONTENT).send();
+  res.noContent();
 });
 
 const refreshTokens = catchAsync(async (req, res) => {
   const tokens = await authService.refreshAuth(req.body.refreshToken);
-  res.send({ ...tokens });
+  res.success({ ...tokens });
 });
 
 const forgotPassword = catchAsync(async (req, res) => {
   const resetPasswordToken = await tokenService.generateResetPasswordToken(req.body.email);
   await emailService.sendResetPasswordEmail(req.body.email, resetPasswordToken);
-  res.status(httpStatus.NO_CONTENT).send();
+  res.noContent();
 });
 
 const resetPassword = catchAsync(async (req, res) => {
   await authService.resetPassword(req.query.token, req.body.password);
-  res.status(httpStatus.NO_CONTENT).send();
+  res.noContent();
 });
 
 const sendVerificationEmail = catchAsync(async (req, res) => {
   const verifyEmailToken = await tokenService.generateVerifyEmailToken(req.user);
   await emailService.sendVerificationEmail(req.user.email, verifyEmailToken);
-  res.status(httpStatus.NO_CONTENT).send();
+  res.noContent();
 });
 
 const verifyEmail = catchAsync(async (req, res) => {
   await authService.verifyEmail(req.query.token);
-  res.status(httpStatus.NO_CONTENT).send();
+  res.noContent();
 });
 
 module.exports = {
--- src/utils/response.js
+++ src/utils/response.js
@@ -41,6 +41,21 @@ const customResponse = (req, res, next) => {
   };
 
   /**
+   * (default status 201)
+   * Success response
+   * @param data
+   * @param message
+   */
+  res.noContent = function (data = {}, message = '') {
+    return res.status(httpStatus.NO_CONTENT).json({
+      status: true,
+      data,
+      message,
+      errors: null,
+    });
+  };
+
+  /**
    * Custom error response
    */
   res.error = function (message = '', errors = {}) {
