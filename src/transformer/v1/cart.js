891c149995c99309728c03d34e2ebc28ee85233c
Duy Thanh <duythanhit203@gmail.com>
2023-09-25 11:51:51 +0700

    [Bugfix] return null when not value transformer cart

--- src/transformer/v1/cart.js
+++ src/transformer/v1/cart.js
@@ -1,23 +1,16 @@
 const getCarts = (data) => {
   const { results, ...meta } = data;
   const products = results.map((i) => {
-    const {
-      _id,
-      productId: { price, images, name, slug, _id: idProduct },
-      quantity,
-      createdAt,
-      updatedAt,
-      deletedAt,
-    } = i.toObject();
+    const { _id, productId, quantity, createdAt, updatedAt, deletedAt } = i.toObject();
     return {
       _id,
       quantity,
       product: {
-        _id: idProduct,
-        name,
-        slug,
-        images: images[0],
-        price,
+        _id: productId?._id || null,
+        name: productId?.name || null,
+        slug: productId?.slug || null,
+        images: productId?.images[0] || null,
+        price: productId?.price || null,
       },
       deletedAt,
       createdAt,
