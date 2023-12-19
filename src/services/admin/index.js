d7cd63615eb71adeb9d6da14810ec8636c5b95ce
Duy Thanh <duythanhit203@gmail.com>
2023-12-19 10:17:02 +0700

    [Bugfix] get product detail update

--- src/services/admin/index.js
+++ src/services/admin/index.js
@@ -1,3 +1,4 @@
 module.exports.adminService = require('./admin.service');
+module.exports.productService = require('./product.service');
 module.exports.analyticService = require('./analytic.service');
 module.exports.orderDetailService = require('./orderDetail.service');
